50083a77df90a17d4db8cfc83cff5c3e57646029
Aelimong <dofla0507@gmail.com>
2023-09-06 16:32:07 +0900

    study(230904): SCSS, Hooks

--- src/App.js
+++ src/App.js
@@ -1,57 +1,15 @@
-import logo from "./logo.svg";
-import styled, { keyframes } from "styled-components";
-
+import UseScss from "./components/UseScss";
+import UseMemoObj from "./components/05_UseMemoObj";
 function App() {
-  const rotate = keyframes`
-  0%{
-    transform: rotate(0deg);
-  }
-  50%{
-    transform: rotate(180deg);
-  }
-  100%{
-    transform: rotate(360deg);
-  }
-  `;
-  const RootDiv = styled.div`
-    width: 100%;
-    height: 100vh;
-    background-color: #282c34;
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
-
-  const AppHeader = styled.header`
-    color: #ffffff;
-  `;
-
-  const AppLogo = styled.img`
-    width: 100%;
-    animation: ${rotate} 7s linear infinite;
-  `;
-
-  const MyA = styled.a`
-    color: #61dafb;
-  `;
-
   return (
-    <RootDiv>
-      <AppHeader>
-        <AppLogo src={logo} alt="app" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <MyA
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </MyA>
-      </AppHeader>
-    </RootDiv>
+    <>
+      {/* <PropsMap arr={dataArr} />
+      <PracticeMap /> */}
+      {/* <PororoObj /> */}
+      {/* <BasicCss color={"blue"} /> */}
+      {/* <UseScss /> */}
+      <UseMemoObj />
+    </>
   );
 }
 
